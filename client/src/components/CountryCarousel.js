77b3b5fba873e1035f7cc7a7f5d5ad09d927157d
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-10-02 21:57:14 +0100

    country images added to carousel

--- client/src/components/CountryCarousel.js
+++ client/src/components/CountryCarousel.js
@@ -2,7 +2,16 @@ import { Carousel } from "react-responsive-3d-carousel";
 import italian from "../assets/images/italian.png";
 import chinese from "../assets/images/chinese.png";
 import irish from "../assets/images/irish.png";
+import american from "../assets/images/american.png";
+import english from "../assets/images/english.png";
+import thai from "../assets/images/thai.png";
+import indian from "../assets/images/indian.png";
+import french from "../assets/images/french.png";
+import mexican from "../assets/images/mexican.png";
+import spanish from "../assets/images/spanish.png";
+
 import { useNavigate } from "react-router-dom";
+import "./CountryCarousel.css"
 
 const CountryCarousel = ({ onImageClick }) => {
   const navigate = useNavigate();
@@ -23,6 +32,44 @@ const CountryCarousel = ({ onImageClick }) => {
       image: irish,
       cuisine: "irish",
     },
+    {
+        country: "United States",
+        image: american,
+        cuisine: "American",
+      },
+      {
+        country: "United Kingdom",
+        image: english,
+        cuisine: "british",
+      },
+      {
+        country: "Thailand",
+        image: thai,
+        cuisine: "thai",
+      },
+      {
+        country: "India",
+        image: indian,
+        cuisine: "indian",
+      },
+      {
+        country: "France",
+        image: french,
+        cuisine: "french",
+      },
+      {
+        country: "Mexico",
+        image: mexican,
+        cuisine: "french",
+      },
+      {
+        country: "Spain",
+        image: spanish,
+        cuisine: "spanish",
+      }
+          
+
+
   ];
   return (
     <Carousel autoPlay={false}>

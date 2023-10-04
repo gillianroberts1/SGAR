b285d2439848d6f61cc6710d0f68829ea9d85aa3
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-10-04 10:49:04 +0100

    Revert "Initial styling"

--- client/src/components/RecipesGrid.js
+++ client/src/components/RecipesGrid.js
@@ -1,74 +1,24 @@
-import "./RecipesGrid.css";
-import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-
-// make the description on grid not too long
-const description_length = 100;
-
-const RecipesGrid = ({ recipes, handleSearch }) => {
-
-  // sorts into alphabetical order
-  recipes.sort((a, b) => a.meal.name.localeCompare(b.meal.name));
-
+const RecipesGrid = ({ recipes, handleButtonClick }) => {
   return (
-    <div className="recipies-grid">
-      
-      <SearchBar handleSearch={handleSearch} />
-      <div className="all-recipes-container">
-        {recipes &&
-          recipes.map((recipe) => (
-            <Link
-              to={`/${recipe._id}`}
-              className="recipe-card"
-              key={recipe._id}
-            >
-              <div className="fav-hearts">
-                {recipe.meal.favourited === true ? (
-                  <span>
-                    <FaHeart />
-                  </span>
-                ) : (
-                  <span>
-                    <FaRegHeart />
-                  </span>
-                )}
-              </div>
-
-              <img
-                className="small-img"
-                src={recipe.meal.image}
-                alt={recipe.meal.name}
-              />
-              <div></div>
-              <p className="recipe-name">{recipe.meal.name}</p>
-              <p className="recipe-description"> 
-              {recipe.meal.description.length > description_length
-                  ? `${recipe.meal.description.slice(0, description_length)}...`
-                  : recipe.meal.description}</p>
-              <p className="recipe-cooktime">
-                {recipe.meal.cooking_time + recipe.meal.preparation_time} mins
-              </p>
-              <p>
-                Vegetarian:{" "}
-                {recipe.meal.vegetarian ? (
-                  <span>&#10003;</span>
-                ) : (
-                  <span>&#10008;</span>
-                )}
-              </p>
-              <p>
-                Vegan:{" "}
-                {recipe.meal.vegan ? (
-                  <span>&#10003;</span>
-                ) : (
-                  <span>&#10008;</span>
-                )}
-              </p>
-              <p className="recipe-country">{recipe.meal.country_of_origin}</p>
-            </Link>
-          ))}
-      </div>
+    <div className="all-recipes-container">
+      {recipes &&
+        recipes.map((recipe) => (
+          <button
+            className="recipe"
+            onClick={() => handleButtonClick(recipe)}
+            key={recipe._id}
+          >
+            <img
+              className="small-img"
+              src={recipe.meal.image}
+              alt={recipe.meal.name}
+            />
+            <p className="recipe-name">{recipe.meal.name}</p>
+            <p className="recipe-description"> {recipe.meal.description}</p>
+            <p className="recipe-cooktime">{recipe.meal.cooking_time} mins</p>
+            <p className="recipe-country">{recipe.meal.country_of_origin}</p>
+          </button>
+        ))}
     </div>
   );
 };

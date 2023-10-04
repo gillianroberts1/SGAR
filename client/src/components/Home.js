3186f571bbbdb88619f41945db50f54fbb117a83
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-10-04 21:14:23 +0100

    prompts added to home screen

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,13 @@
 import CountryCarousel from "./CountryCarousel";
 import RecipesGrid from "./RecipesGrid";
+import "./Home.css"
 
 const Home = ({randomRecipes}) => {
   return (
     <>
+    <p className="cuisine"> What's your favourite cuisine?</p>
       <CountryCarousel />
+      <p className="suggestion">Can't decide? How about one of these...</p>
       <RecipesGrid recipes={randomRecipes} showSearchBar={false}/>
     </>
   );

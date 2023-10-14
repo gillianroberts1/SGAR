736cb1be490ea18597bb28b226b546e64c4581ae
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-10-14 22:35:37 +0100

    server updated

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router.js");
 
+MongoClient.connect(  "mongodb+srv://gillianroberts:nqshhUiLnRkhZTNh@cluster0.7ctdfyx.mongodb.net/",
 
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
-    .then((client) => { 
-        const db = client.db('recipes_project');
-        const recipesCollection = db.collection('recipes');
-        const recipesRouter = createRouter(recipesCollection);
-        app.use('/api/recipes', recipesRouter);
-        const favouritesCollection = db.collection('favourites')
-        const favouritesRouter = createRouter(favouritesCollection)
-        app.use('/api/favourites', favouritesRouter);
-
-    })
-    .catch(console.err);
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    const db = client.db("recipes_project");
+    const recipesCollection = db.collection("recipes");
+    const recipesRouter = createRouter(recipesCollection);
+    app.use("/api/recipes", recipesRouter);
+    const favouritesCollection = db.collection("favourites");
+    const favouritesRouter = createRouter(favouritesCollection);
+    app.use("/api/favourites", favouritesRouter);
+  })
+  .catch(console.err);
 
 app.listen(9000, function () {
-    console.log(`Listening on port ${ this.address().port }`);
+  console.log(`Listening on port ${this.address().port}`);
 });

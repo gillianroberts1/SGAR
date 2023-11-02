2c54c339d63eb9ee0a15571af8655b9a67f71ee0
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2023-11-02 15:41:43 +0000

    refresh button added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,11 +3,24 @@ import RecipesGrid from "./RecipesGrid";
 import "./Home.css";
 
 const Home = ({ randomRecipes }) => {
+  const refreshPage = () => {
+    window.location.reload(true);
+  };
+
   return (
     <>
       <p className="cuisine"> What's your favourite cuisine?</p>
       <CountryCarousel />
       <p className="suggestion">Can't decide? How about one of these...</p>
+      {randomRecipes.length === 0 ? (
+        <button className="refresh load" onClick={refreshPage}>
+          Click to load recipes
+        </button>
+      ) : (
+        <button className="refresh more" onClick={refreshPage}>
+          Suggest something else
+        </button>
+      )}
       <RecipesGrid recipes={randomRecipes} showSearchBar={false} />
     </>
   );
